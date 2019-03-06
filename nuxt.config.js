5e4cd4c40b25a46a61eebfe0e9a7c16d080873ae
Sarchieo <hxbnzy@gmail.com>
2019-03-06 19:09:36 +0800

    no message

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  // mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ice/3.6.3/Ice.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ice/3.6.3/Glacier2.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ice/3.6.3/IceGrid.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ice/3.6.3/IceStorm.js' },
+      { src: 'http://39.108.85.159:8080/wlq/info.js' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'ant-design-vue/dist/antd.css',
+    'assets/main.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/antd-ui',
+    '@/plugins/vue-lazyload.js',
+    { src: '~plugins/fs-plugin.js', ssr: false },
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // console.log('webpack config:', config)
+     if(ctx.isClient) {
+      config.module.unknownContextCritical = false
+     }
+    }
+  }
+}
--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,4 @@
 export const login = (context, code) => {
-  debugger
   // 调用后台登录
   return new Promise(((resolve, reject) => {
     resolve(0)
