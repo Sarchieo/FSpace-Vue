46c6392c28708c1ec1befe4d80e4b23c8aac82ae
soul <464125574@QQ.com>
2019-03-18 17:18:49 +0800

    药店资质

--- pages/user/personal.vue
+++ pages/user/personal.vue
@@ -3,25 +3,29 @@
     <a-layout>
        <f-space-header type='home'></f-space-header>
        <a-layout-content class="base">
-         <a-breadcrumb separator=">">
+          <a-breadcrumb separator=">" class="crumbs">
             <a-breadcrumb-item>首页</a-breadcrumb-item>
             <a-breadcrumb-item>我的一块</a-breadcrumb-item>
           </a-breadcrumb>
           <!-- 头部内容 -->
           <div class='header-personal'>
             <div class="left">
-              <div>
-                <a-icon type="user" />
+              <p class="first">
+                <a-icon type="profile" />
                 长沙大药房
-              </div>
-               <div>
-                 <a-icon type="user" />
+              </p>
+               <p>
+                 <a-icon type="profile" />
                 周星星
-              </div>
-               <div>
-                 <a-icon type="user" />
+              </p>
+               <p>
+                 <a-icon type="profile" />
                 17621503679
-              </div>
+              </p>
+            </div>
+            <div class="right">
+              <p><span>优惠券：</span>3张</p>
+              <p><span>积份：</span>268</p>
             </div>
           </div>
           <div class="content">
@@ -32,27 +36,27 @@
                 mode="vertical"
                 @click="handleClick"
               >
-              <a-menu-item key="/user/personal">
-                <a-icon type="mail" />
-                我的药店
-              </a-menu-item>
-              <a-menu-item key="/user/personal/invoice">
-                <a-icon type="calendar" />
-                我的发票
-              </a-menu-item>
-              <a-menu-item key="/user/personal/receiver">
-                <a-icon type="mail" />
-                我的收货人
-              </a-menu-item>
-              <a-menu-item key="/user/personal/collection">
-                <a-icon type="calendar" />
-                我的收藏
-              </a-menu-item>
-               <a-menu-item key="/user/personal/service">
-                <a-icon type="calendar" />
-                售后服务
-              </a-menu-item>
-            </a-menu>
+                <a-menu-item key="/user/personal">
+                  <a-icon type="mail" />
+                  我的药店
+                </a-menu-item>
+                <a-menu-item key="/user/personal/invoice">
+                  <a-icon type="calendar" />
+                  我的发票
+                </a-menu-item>
+                <a-menu-item key="/user/personal/receiver">
+                  <a-icon type="mail" />
+                  我的收货人
+                </a-menu-item>
+                <a-menu-item key="/user/personal/collection">
+                  <a-icon type="calendar" />
+                  我的收藏
+                </a-menu-item>
+                <a-menu-item key="/user/personal/service">
+                  <a-icon type="calendar" />
+                  售后服务
+                </a-menu-item>
+              </a-menu>
             </div>
             <div class="right">
               <nuxt-child></nuxt-child>
@@ -88,6 +92,12 @@ export default {
 };
 </script> 
 <style lang="less" scoped>
+@import "../../components/fspace-ui/container/index.less";
+@import "../../components/fspace-ui/button/index.less";
+
+  .crumbs {
+    .p-size (50px,50px,16px,left,0px,#999999);
+  }
   .base {
     width: 1200px;
     left:0;
@@ -103,14 +113,36 @@ export default {
     background: white;
     border: solid 1px #ffffff;
     .left {
+      float: left;
       width: 25%;
-      height: 140px;
-      text-align:center;  
-      div {
-        width: 250px;
-        height: 32px;
-        margin: 0 auto;
-        background: yellow;
+      height: 120px;
+      text-align:center;
+      margin-top: 10px;
+      .first {
+        background: #fbdedd;
+      }
+      p {
+       .p-size (40px,40px,18px,left,20px,#333333);
+       i {
+         margin-right: 15px;
+         color: #ed3025;
+       }
+      }
+    }
+    .right {
+      float: left;
+      width: 25%;
+      height: 120px;
+      margin-top: 10px;
+      margin-left: 80px;
+      border-left: 1px solid #e0e0e0;
+      p {
+        .p-size (60px,60px,18px,left,20px,#ed3025);
+        span {
+          .container-size (inline-block,110px,auto,0,0);
+          color: #333333;
+        }
+
       }
     }
   }
--- pages/user/personal/index.vue
+++ pages/user/personal/index.vue
@@ -1,15 +1,206 @@
 <template>
-  <div>
-    首页
+  <div class="certificates-box">
+    <div class="certificates-content">
+        <h2 class="pharmacy-info">药店信息</h2>
+        <a-form
+          :form="form"
+          @submit="handleSubmit"
+        >
+        <!-- 表单元素 -->
+
+        <h2 class="certificate-title">药店资质</h2>
+        <div class="photo-box">
+          <div class="business-license">
+             <a-upload
+                name="avatar"
+                listType="picture-card"
+                class="avatar-uploader"
+                :showUploadList="false"
+                action="//jsonplaceholder.typicode.com/posts/"
+                :beforeUpload="beforeUpload"
+                @change="handleChange"
+              >
+                <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+                <div v-else>
+                    <a-icon :type="loading ? 'loading' : 'plus'" />
+                    <div class="ant-upload-text">Upload</div>
+                </div>
+              </a-upload>
+          </div>
+          <div class="management">
+            <a-upload
+                name="avatar"
+                listType="picture-card"
+                class="avatar-uploader"
+                :showUploadList="false"
+                action="//jsonplaceholder.typicode.com/posts/"
+                :beforeUpload="beforeUpload"
+                @change="handleChange"
+              >
+                <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+                <div v-else>
+                    <a-icon :type="loading ? 'loading' : 'plus'" />
+                    <div class="ant-upload-text">Upload</div>
+                </div>
+              </a-upload>
+          </div>
+          <div>
+            <a-upload
+                name="avatar"
+                listType="picture-card"
+                class="avatar-uploader"
+                :showUploadList="false"
+                action="//jsonplaceholder.typicode.com/posts/"
+                :beforeUpload="beforeUpload"
+                @change="handleChange"
+              >
+                <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+                <div v-else>
+                    <a-icon :type="loading ? 'loading' : 'plus'" />
+                    <div class="ant-upload-text">Upload</div>
+                </div>
+              </a-upload>
+          </div>
+        </div>
+        <p class="certificate-text">
+          <span>营业执照</span>
+          <span>药品经营许可证</span>
+          <span>GSP证书</span>
+        </p>
+        <p class="certificate-fill">
+          <span>(必填)</span>
+          <span>(必填)</span>
+          <span>(必填)</span>
+        </p>
+        <button class="save-btn">保存提交</button>
+        </a-form>
+    </div>
   </div>
 </template>
 <script>
+function getBase64 (img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
 export default {
-  
+    data() {
+        return {
+            loading: false,
+            imageUrl: '',
+            options: [{
+                    value: 'zhejiang',
+                    label: 'Zhejiang',
+                    children: [{
+                    value: 'hangzhou',
+                    label: 'Hangzhou',
+                    children: [{
+                        value: 'xihu',
+                        label: 'West Lake',
+                    }],
+                    }],
+                }, {
+                    value: 'jiangsu',
+                    label: 'Jiangsu',
+                    children: [{
+                    value: 'nanjing',
+                    label: 'Nanjing',
+                    children: [{
+                        value: 'zhonghuamen',
+                        label: 'Zhong Hua Men',
+                    }],
+                    }],
+                }]
+                    }
+    },
+    methods: {
+      onChange(value) {
+        console.log(value);
+      },
+       handleChange (info) {
+      if (info.file.status === 'uploading') {
+        this.loading = true
+        return
+      }
+      if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, (imageUrl) => {
+          this.imageUrl = imageUrl
+          this.loading = false
+        })
+      }
+    },
+    beforeUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        this.$message.error('You can only upload JPG file!')
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2
+      if (!isLt2M) {
+        this.$message.error('Image must smaller than 2MB!')
+      }
+      return isJPG && isLt2M
+    }
+  }
 }
 </script>
-<style>
+<style scoped lang="less">
+@import "../../../components/fspace-ui/container/index.less";
+@import "../../../components/fspace-ui/button/index.less";
+ .avatar-uploader > .ant-upload {
+    width: 128px;
+    height: 128px;
+  }
+  .ant-upload-select-picture-card i {
+    font-size: 32px;
+    color: #999;
+  }
 
+  .ant-upload-select-picture-card .ant-upload-text {
+    margin-top: 8px;
+    color: #666;
+  }
+  .certificates-box {
+    .container-size(block,985px,1015px,0,0px);
+    padding: 40px 0;
+    .certificates-content {
+     .container-size(block,730px,935px,0 auto,0px);
+     .pharmacy-info {
+       .p-size(60px,60px,20px,left,0px,#333333);
+     }
+     .photo-box {
+        .container-size(inline-block,730px,345px,0,0px);
+        div {
+          .container-size(inline-block,210px,280px,0,0px);
+          .position(relative,0px,0px);
+          float:left;
+        }
+        .business-license,.management {
+            margin-right: 40px;
+        }
+     }
+     .certificate-title {
+       .p-size(60px,60px,20px,left,0px,#333333);
+     }
+     .certificate-text {
+       .p-size(50px,50px,20px,center,0px,#333333);
+       span {
+         .container-size(inline-block,239px,50px,0,0px);
+       }
+     }
+     .certificate-fill {
+        .p-size(50px,50px,16px,center,0px,#ed3025);
+       span {
+         .container-size(inline-block,239px,50px,0,0px);
+       }
+     }
+     .save-btn {
+       .button-display(block, 15px auto 0 auto);
+       .button-size (120px,48px,48px,18px,0px,5px);
+       .button-color(1px solid transparent,#ed3025,#ffffff);
+     }
+    }
+  }
 </style>
 
 
