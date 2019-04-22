d1a2b3e2b2b7506904adc361ab1653e75366067c
chenyuqiong <1184289767@qq.com>
2019-04-22 11:35:15 +0800

    我的订单查询

--- pages/user/personal/myorder.vue
+++ pages/user/personal/myorder.vue
@@ -150,7 +150,7 @@ export default {
       iRequest.cls = "OrderInfoModule";
       iRequest.method = "queryOrders";
       iRequest.param.token = localStorage.getItem("identification");
-      iRequest.param.arrays = [this.ostatus,this.storeInfo.storeId];
+      iRequest.param.arrays = [this.ostatus];
       iRequest.param.pageIndex = this.currentIndex;
       iRequest.param.pageNumber = 10;
       this.$refcallback(
