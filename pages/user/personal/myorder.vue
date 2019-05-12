6af2c9031371b144d8ec6a19626743577eb30355
chenyuqiong <1184289767@qq.com>
2019-05-12 12:22:18 +0800

    线下订单支付修改

--- pages/user/personal/myorder.vue
+++ pages/user/personal/myorder.vue
@@ -440,7 +440,6 @@ export default {
     },
       //是否可以取消订单
       canCancel(item) {
-        console.log("item---- " + JSON.stringify(item))
         let date = item.odate + " " + item.otime;
         date = date.substring(0,19);
         date = date.replace(/-/g,'/');
@@ -448,7 +447,7 @@ export default {
         var timestampNow = parseInt(new Date().getTime());    // 当前时间戳
         var times = timestampNow - timestamp;
         var thirtyMin = 30 * 60 * 1000;
-        if (item.payway == 4 && times < thirtyMin){
+        if (item.payway == 4 && item.ostatus ===0 && times < thirtyMin){
             return true
         }
         if (item.payway == 5 && item.ostatus === 1 && times < thirtyMin) {
@@ -488,7 +487,11 @@ export default {
       let _this = this;
       let iRequest = new inf.IRequest();
       iRequest.cls = "TranOrderOptModule";
-      iRequest.method = "cancelOrder";
+      if (item.payway == 4 || item.payway == 5){
+          iRequest.method = "cancelOffLineOrder";
+      } else {
+          iRequest.method = "cancelOrder";
+      }
       iRequest.param.token = localStorage.getItem("identification");
       iRequest.param.json = JSON.stringify({
         orderno: item.orderno,
