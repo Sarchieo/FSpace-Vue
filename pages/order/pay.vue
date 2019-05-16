91fc7a0c24c004ec74f16a8d052b2359349c5755
肖攀 <464125574@qq.com>
2019-05-16 20:15:22 +0800

    支付页面

--- pages/order/pay.vue
+++ pages/order/pay.vue
@@ -6,44 +6,78 @@
         <div class="stets">
           <a-steps :current="2">
             <a-step>
-              <template slot="title">
-                我的采购单
-              </template>
+              <template slot="title">我的采购单</template>
               <span slot="description"></span>
             </a-step>
-            <a-step title="采购单信息" />
-            <a-step title="订单付款" />
-            <a-step title="订单完成" />
+            <a-step title="采购单信息"/>
+            <a-step title="订单付款"/>
+            <a-step title="订单完成"/>
           </a-steps>
         </div>
+        <div>
           <div class="pay-info">
-              <p class="pay-text">订单提交成功，请尽快付款！订单号：<span>{{ orderno }}</span></p>
-              <p class="urge">请您在半小时内完成支付，否则订单会被自动取消。</p>
-              <div class="actual-pay">
-                  <span>实付：</span> ￥ {{ payamt }}元
-              </div>
+            <p class="pay-text">
+              订单号：
+              <span>{{ orderno }}</span>
+            </p>
+
+            <p class="actual-pay">
+              <span>实付金额:</span>
+              ￥ {{ payamt }}元
+            </p>
+            <p class="invo-p">
+              <span>发票信息：</span> 普通发票
+            </p>
+            <p class="time-p">
+              <span>下单时间：{{payInfo.odate}} {{payInfo.otime}}</span>
+            </p>
           </div>
           <div class="pay-type">
-              <p class="pay-title">请选择支付方式</p>
-              <div class="pay-pick">
-                  <p>
-                    <a-radio-group v-model="value">
-                      <a-radio :value="1"><a-icon type="alipay-circle" class="blue"/></a-radio>
-                      <a-radio :value="2"><a-icon type="wechat" class="green"/></a-radio>
-                      <a-radio :value="3" class="line-down">线下转账</a-radio>
-                      <a-radio :value="4" class="line-down">线下到付</a-radio>
-                    </a-radio-group>
-                  </p>
-              </div>
+            <p class="pay-title">请选择支付方式</p>
+            <div class="pay-pick">
+              <p>
+                <a-radio-group v-model="value">
+                  <a-radio :value="1">
+                    <a-icon type="alipay-circle" class="blue"/>
+                  </a-radio>
+                  <a-radio :value="2">
+                    <a-icon type="wechat" class="green"/>
+                  </a-radio>
+                  <a-radio :value="3" class="line-down">线下即付</a-radio>
+                  <a-radio :value="4" class="line-down">线下到付</a-radio>
+                </a-radio-group>
+              </p>
+            </div>
           </div>
           <div class="pay-btn">
-              <p class="btn-box">
-                  <a-button :disabled="!isPay" type="primary" @click="prePay()" v-if="this.value === 1 || this.value === 2">立即支付</a-button>
-                  <a-button :disabled="!isPay" type="primary" @click="offlinePay('4')" v-if="this.value === 3">提交订单</a-button>
-                  <a-button :disabled="!isPay" type="primary" @click="offlinePay('5')" v-if="this.value === 4">提交订单</a-button>
-              </p>
-              <p class="surplus-time">剩余付款时间：<span>{{ h }}</span>小时<span>{{ m }}</span>分钟<span>{{ s }}</span>秒</p>
+            <p class="btn-box">
+              <a-button
+                :disabled="!isPay"
+                type="primary"
+                @click="prePay()"
+                v-if="this.value === 1 || this.value === 2"
+              >立即支付</a-button>
+              <a-button
+                :disabled="!isPay"
+                type="primary"
+                @click="offlinePay('4')"
+                v-if="this.value === 3"
+              >立即支付</a-button>
+              <a-button
+                :disabled="!isPay"
+                type="primary"
+                @click="offlinePay('5')"
+                v-if="this.value === 4"
+              >确定付款</a-button>
+            </p>
+            <p class="surplus-time">
+              剩余付款时间：
+              <span>{{ h }}</span>小时
+              <span>{{ m }}</span>分钟
+              <span>{{ s }}</span>秒
+            </p>
           </div>
+        </div>
       </a-layout-content>
       <f-space-footer></f-space-footer>
     </a-layout>
@@ -64,18 +98,19 @@ export default {
   },
   data() {
     return {
-     value: 1,
-     payamt: 0,
-     h: 0,
-     m: 0,
-     s: 0,
-     orderno: '',
-     isPay: false
+      value: 1,
+      payamt: 0,
+      h: 0,
+      m: 0,
+      s: 0,
+      orderno: "",
+      isPay: false,
+      payInfo: {}
     };
   },
   mounted() {
-    this.orderno = this.$route.query.orderno
-    this.showPayInfo()
+    this.orderno = this.$route.query.orderno;
+    this.showPayInfo();
   },
   methods: {
     showPayInfo() {
@@ -87,28 +122,28 @@ export default {
         orderno: this.orderno,
         compid: this.storeInfo.comp.storeId
       });
-      iRequest.param.token = localStorage.getItem("identification")
+      iRequest.param.token = localStorage.getItem("identification");
       this.$refcallback(
         this,
-        "orderServer" + Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
+        "orderServer" +
+          Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
         iRequest,
-        new this.$iceCallback(
-          function result(result) {
-            if (result.code === 200) {
-              // 余额完全支付
-              if(result.data.balflag === 1) {
-                _this.balAllPay()
-              } else {
-                _this.payamt = result.data.payamt
-                _this.isPay = true
-                _this.secondKill(
+        new this.$iceCallback(function result(result) {
+          if (result.code === 200) {
+            _this.payInfo = result.data;
+            // 余额完全支付
+            if (result.data.balflag === 1) {
+              _this.balAllPay();
+            } else {
+              _this.payamt = result.data.payamt;
+              _this.isPay = true;
+              _this.secondKill(
                 _this.stringToDate(result.data.now),
-                  result.data.otime
-                );
-              }
+                result.data.otime
+              );
             }
           }
-        )
+        })
       );
     },
     balAllPay() {
@@ -118,25 +153,24 @@ export default {
       iRequest.method = "balAllPay";
       iRequest.param.json = JSON.stringify({
         orderno: this.orderno,
-        compid: this.storeInfo.comp.storeId,
+        compid: this.storeInfo.comp.storeId
       });
-      iRequest.param.token = localStorage.getItem("identification")
+      iRequest.param.token = localStorage.getItem("identification");
       this.$refcallback(
         this,
-        "orderServer" + Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
+        "orderServer" +
+          Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
         iRequest,
-        new this.$iceCallback(
-          function result(result) {
-            if (result.code === 200) {
-              _this.$router.push({
-                path: '/order/pay-complete',
-                query: {
-                  orderno: _this.orderno
-                }
-              })
-            }
+        new this.$iceCallback(function result(result) {
+          if (result.code === 200) {
+            _this.$router.push({
+              path: "/order/pay-complete",
+              query: {
+                orderno: _this.orderno
+              }
+            });
           }
-        )
+        })
       );
     },
     // 线上支付
@@ -150,25 +184,24 @@ export default {
         compid: this.storeInfo.comp.storeId,
         paytype: "alipay"
       });
-      iRequest.param.token = localStorage.getItem("identification")
+      iRequest.param.token = localStorage.getItem("identification");
       this.$refcallback(
         this,
-        "orderServer" + Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
+        "orderServer" +
+          Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
         iRequest,
-        new this.$iceCallback(
-          function result(result) {
-            if (result.code === 200) {
-              _this.$router.push({
-                path: '/order/payment',
-                query: {
-                  url: result.data,
-                  payamt: _this.payamt,
-                  orderno: _this.orderno
-                }
-              })
-            }
+        new this.$iceCallback(function result(result) {
+          if (result.code === 200) {
+            _this.$router.push({
+              path: "/order/payment",
+              query: {
+                url: result.data,
+                payamt: _this.payamt,
+                orderno: _this.orderno
+              }
+            });
           }
-        )
+        })
       );
     },
     // 线下
@@ -182,24 +215,23 @@ export default {
         compid: this.storeInfo.comp.storeId,
         paytype: type
       });
-      iRequest.param.token = localStorage.getItem("identification")
+      iRequest.param.token = localStorage.getItem("identification");
       this.$refcallback(
         this,
-        "orderServer" + Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
+        "orderServer" +
+          Math.floor((this.storeInfo.comp.storeId / 8192) % 65535),
         iRequest,
-        new this.$iceCallback(
-          function result(result) {
-            if (result.code === 200) {
-              _this.$router.push({
-                path: '/order/line-down',
-                query: {
-                  type: type,
-                  orderno: _this.orderno
-                }
-              })
-            }
+        new this.$iceCallback(function result(result) {
+          if (result.code === 200) {
+            _this.$router.push({
+              path: "/order/line-down",
+              query: {
+                type: type,
+                orderno: _this.orderno
+              }
+            });
           }
-        )
+        })
       );
     },
     stringToDate(str) {
@@ -215,7 +247,7 @@ export default {
       var date = new Date(year, month, day, hour, minute, second);
       return date;
     },
-     // 设置倒计时
+    // 设置倒计时
     secondKill(date, eDate) {
       let endDate = this.stringToDate(
         date.getFullYear() +
@@ -226,24 +258,24 @@ export default {
           " " +
           eDate
       );
-      let times = Math.floor((endDate - date)/1000) + 1800;
+      let times = Math.floor((endDate - date) / 1000) + 1800;
       let _this = this;
       if (times >= 0) {
         let timer;
         timer = setInterval(function() {
-          times --;
-          _this.h = Math.floor(times/60/60);
-          _this.m = Math.floor(times/60)%60;
-          _this.s = times%60;
+          times--;
+          _this.h = Math.floor(times / 60 / 60);
+          _this.m = Math.floor(times / 60) % 60;
+          _this.s = times % 60;
           if (times <= 0) {
             clearInterval(timer);
-            _this.isPay = false
+            _this.isPay = false;
           }
         }, 1000);
         if (times >= 0) {
-          _this.isPay = true
+          _this.isPay = true;
         } else {
-          _this.isPay = false
+          _this.isPay = false;
         }
       }
     }
@@ -298,84 +330,101 @@ li {
 #components-layout-demo-basic > .ant-layout:last-child {
   margin: 0;
 }
-.pay-info{
-  .container-size(block,1190px,120px,0 auto,0px);
+.pay-explain {
+  .container-size(block, 1190px, 60px, 0 auto, 0px);
+  line-height: 60px;
+  background: #f2f2f2;
+  text-align: center;
+  font-size: 20px;
+  color: #666;
+}
+.pay-info {
+  .container-size(block, 1190px, 120px, 0 auto, 0px);
   border: 1px solid #e0e0e0;
-  padding:30px;
+  padding: 20px;
+  padding-left: 30px;
   margin-bottom: 20px;
-  .pay-text{
-      display:inline-block;
-      width: 600px;
-      .p-size(30px,30px,16px,left,0px,#333333);
-      span{
-          color: #ed2f26;
-      }
+  .invo-p,.time-p{
+    float: left;
+    width: 300px;
+    font-size: 16px;
+    color: #333;
   }
-  .urge{
-      display:inline-block;
-      width: 600px;
-      .p-size(30px,30px,14px,left,0px,#999999);
-  }
-  .actual-pay{
-      .container-size(inline-block,300px,50px,0 auto,0px);
-      float:right;
-      text-align: right;
-      font-size: 16px;
+  .pay-text {
+    display: inline-block;
+    float: left;
+    width: 300px;
+    .p-size(30px, 30px, 16px, left, 0px, #333333);
+    span {
       color: #ed2f26;
-      span{
-          color: #333333;
-      }
+    }
+  }
+  .urge {
+    display: inline-block;
+    width: 600px;
+    .p-size(30px, 30px, 14px, left, 0px, #999999);
+  }
+  .actual-pay {
+    .container-size(inline-block, 800px, 30px, 0 auto, 0px);
+    float: left;
+    line-height: 30px;
+    text-align: left;
+    font-size: 16px;
+    color: #ed2f26;
+    span {
+      color: #333333;
+    }
   }
 }
-.pay-type{
-  .container-size(block,1190px,175px,0 auto,0px);
+.pay-type {
+  .container-size(block, 1190px, 175px, 0 auto, 0px);
   border: 1px solid #e0e0e0;
   margin-bottom: 20px;
-  .pay-title{
-      .p-size(50px,50px,18px,left,30px,#666666);
-      background: #e0e0e0;
+  .pay-title {
+    .p-size(50px, 50px, 18px, left, 30px, #666666);
+    background: #e0e0e0;
   }
-  .pay-pick{
-    .container-size(block,1190px,125px,0 auto,0px);
-    p{
-        .p-size(125px,125px,18px,left,0px,#666666);
-        padding-left: 30px;
-        i{
-            text-align: left;
-            font-size: 30px;
-        }
-    }
-  }
-}
-.pay-btn{
-    .container-size(block,1190px,140px,0 auto,0px);
-    padding-top: 35px;
-    .btn-box{
-        .p-size(50px,50px,16px,right,0px,#666666);
-        padding-left: 990px;
-      button{
-        display: block;
-        .button-size(200px,50px,50px,16px,0px,5px);
-        .button-color(1px solid transparent,#ed2f26,#ffffff);
+  .pay-pick {
+    .container-size(block, 1190px, 125px, 0 auto, 0px);
+    p {
+      .p-size(125px, 125px, 18px, left, 0px, #666666);
+      padding-left: 30px;
+      i {
+        text-align: left;
+        font-size: 30px;
       }
     }
+  }
 }
-.blue{
-    margin-right: 40px;
-    color: #3189f5;
+.pay-btn {
+  .container-size(block, 1190px, 140px, 0 auto, 0px);
+  padding-top: 35px;
+  .btn-box {
+    .p-size(50px, 50px, 16px, right, 0px, #666666);
+    padding-left: 990px;
+    button {
+      display: block;
+      .button-size(200px, 50px, 50px, 16px, 0px, 5px);
+      .button-color(1px solid transparent, #ed2f26, #ffffff);
+    }
+  }
 }
-.green{
-    color: #09BB07;
+.blue {
+  margin-right: 40px;
+  color: #3189f5;
 }
-.surplus-time{
-    display: block;
-    .p-size(40px,40px,16px,right,0px,#666666);
+.green {
+  color: #09bb07;
 }
-.stets{
+.surplus-time {
+  display: block;
+  .p-size(40px, 40px, 16px, right, 0px, #666666);
+}
+.stets {
   .container-size(block, 600px, 100px, 0 auto, 0);
   margin-top: 30px;
 }
-.line-down{
+.line-down {
   margin-left: 30px;
 }
 </style>
